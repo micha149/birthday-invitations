d369523fb870ae4861d3c059f6749482aebc46ae
Michael van Engelshoven <michael@van-engelshoven.de>
2023-08-20 16:54:13 +0200

    Add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
+import stylesheet from "~/styles.css";
 import type { LinksFunction } from "@remix-run/node";
 import {
   Links,
@@ -10,6 +11,7 @@ import {
 } from "@remix-run/react";
 
 export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesheet },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
