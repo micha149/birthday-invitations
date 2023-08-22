c67999c51fa89c13a1bd09901a7cc3bed43d0aa8
Michael van Engelshoven <michael@van-engelshoven.de>
2023-08-22 23:56:07 +0200

    Prevent some line breads in salutation

--- app/util/salutation.ts
+++ app/util/salutation.ts
@@ -1,5 +1,5 @@
 export const createSalutation = (people: {name: string, isMale: boolean}[]): string => {
-    const seperated = people.map(p => `liebe${p.isMale ? 'r' : ''} ${p.name}`);
+    const seperated = people.map(p => `liebe${p.isMale ? 'r' : ''} ${p.name}`);
     const combined = seperated.join(', ') + '.';
 
     return combined.charAt(0).toUpperCase() + combined.slice(1);
