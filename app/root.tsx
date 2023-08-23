18798926cf8baae50d3efd9701c965983a2a39b4
Michael van Engelshoven <michael@van-engelshoven.de>
2023-08-23 16:15:53 +0200

    Add favicon and preview image

--- app/root.tsx
+++ app/root.tsx
@@ -1,6 +1,6 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
 import stylesheet from "~/styles.css";
-import type { LinksFunction } from "@remix-run/node";
+import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -10,8 +10,25 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 
+export const meta: V2_MetaFunction = () => {
+  return [
+    { title: "Ich werde 40!" },
+    {
+      name: "description",
+      content: "Geburtstagseinladung von Michael",
+    },
+    {
+      property: "og:image",
+      content: "https://party.michael-van-engelshoven.de/preview.webp",
+    },
+  ];
+};
+
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: stylesheet },
+  { rel: "image_src", href: "/preview.webp"},
+  { rel: "icon", href: "/favicon.svg", type: "image/svg+xml" },
+  { rel: "icon", href: "/favicon-32x32.png", type: "image/png" },
   ...(cssBundleHref ? [{ rel: "stylesheet", href: cssBundleHref }] : []),
 ];
 
